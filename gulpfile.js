4b07a9b109dacdc99c58f605540973ad2746f619
evgkit <kitaev.ea@gmail.com>
2016-02-11 14:24:49 +0300

    Add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,58 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    del = require('del');
 
-gulp.task('concatScripts', function() {
-    gulp.src([
-        'js/jquery.js',
-        'js/sticky/jquery.sticky.js',
-        'js/main.js'
-    ])
+gulp.task('concat', function () {
+    return gulp.src([
+            'resource/js/jquery.js',
+            'resource/js/sticky/jquery.sticky.js',
+            'resource/js/main.js'
+        ])
+        .pipe(sourcemaps.init())
         .pipe(concat('application.js'))
-        .pipe(gulp.dest('js'));
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('assets/js'));
 });
 
-gulp.task('minifyScripts', function() {
-    gulp.src('js/application.js')
+gulp.task('minify', [
+    'concat'
+], function () {
+    return gulp.src('assets/js/application.js')
         .pipe(uglify())
         .pipe(rename('application.min.js'))
-        .pipe(gulp.dest('js'));
+        .pipe(gulp.dest('assets/js'));
 });
 
-gulp.task('default', ['wow'], function() {
-    console.log('default task!');
+gulp.task('sass', function () {
+    return gulp.src('resource/scss/application.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('assets/css'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch('resource/scss/**/*.scss', ['sass']);
+    gulp.watch('resource/js/**/*.js', ['minify']);
+});
+
+gulp.task('del', function() {
+    del('assets');
+});
+
+gulp.task('build', [
+    'del'
+], function() {
+    gulp.start(['minify', 'sass']);
+    return gulp.src(['resource/img/**', 'resource/fonts/**'], { base: 'resource'})
+        .pipe(gulp.dest('assets'));
+});
+
+gulp.task('default', [
+    'build',
+    'watch'
+]);
